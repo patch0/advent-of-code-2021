b7f5225e937561c794f6a5a70c10c61ac0160873
Patrick J Cherry <patch@headtube.dominoid.net>
2021-12-03 09:08:31 +0000

    Refactor day 2 part 2 to use metaprogramming

--- 02/part2.rb
+++ 02/part2.rb
@@ -1,26 +1,23 @@
 class Part2
+  def initialize
+    @x = @y = @aim = 0
+  end
+
   def run
-    x = y = aim = 0
-
-    instructions.each do |d, c|
-      c = c.to_i
-      case d
-      when 'forward'
-        x += c
-        y += aim * c
-      when 'up'
-        aim -= c
-      when 'down'
-        aim += c
-      end
-    end
-    x * y
+    instructions.each { |i| send(*i) }
+    @x * @y
   end
 
-  def instructions
-    @instructions ||= STDIN.readlines.map(&:split)
-  end
+  def instructions = $stdin.readlines.map(&:split).map { |a, b| [a, b.to_i] }
 
+  def down(amt) = @aim += amt
+
+  def up(amt) = @aim -= amt
+
+  def forward(amt)
+    @x += amt
+    @y += @aim * amt
+  end
 end
 
 pp Part2.new.run
