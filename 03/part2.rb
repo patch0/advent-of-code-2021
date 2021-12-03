da8ada0b1fff3718d2f319fd101363a04c38c3f7
Patrick J Cherry <patch@headtube.dominoid.net>
2021-12-03 09:37:29 +0000

    Refactor part 2 to be a bit neater

--- 03/part2.rb
+++ 03/part2.rb
@@ -1,37 +1,25 @@
-module MostCommonPos
-  refine Array do
-    def most_common_bit_at(pos)
-      mask = 1 << (pos - 1)
-      count { |r| (r & mask).positive? } >= (count / 2.0) ? 1 : 0
-    end
-  end
-end
-
 module Weed
   refine Array do
-    def weed(mask, bit)
+    def weed(pos, common: true)
       return if count == 1
 
+      mask = 1 << (pos - 1)
+      bit = count { |r| (r & mask).positive? } >= (count / 2.0) ? 1 : 0
+      bit ^= 1 unless common
       delete_if { |r| (bit == 1) == (r & mask).zero? }
     end
   end
 end
 
 class Part2
-  using MostCommonPos
   using Weed
 
   def run
     n_bits = input.first.length
 
     (n_bits..0).step(-1).each do |pos|
-      mask = 1 << (pos - 1)
-
-      common_bit = o2_readings.most_common_bit_at(pos)
-      o2_readings.weed(mask, common_bit)
-
-      uncommon_bit = co2_readings.most_common_bit_at(pos) ^ 1
-      co2_readings.weed(mask, uncommon_bit)
+      o2_readings.weed(pos)
+      co2_readings.weed(pos, common: false)
     end
 
     o2_readings.first * co2_readings.first
