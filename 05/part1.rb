ad9048455eb08cb3382f8383b591de0c94a213d4
Patrick J Cherry <patch@headtube.dominoid.net>
2021-12-05 19:35:47 +0000

    Day 5, parts 1 and 2
    Took longer than it should have done.

---
+++ 05/part1.rb
@@ -0,0 +1,41 @@
+class Line
+  attr_reader :coords
+
+  def initialize(coords)
+    @coords = coords
+  end
+
+  def points
+    @points ||= if horizontal?
+                  r = (coords[0]..coords[2]).step(coords[2] <=> coords[0]).to_a
+                  r.zip(Array.new(r.length, coords[1]))
+                elsif vertical?
+                  r = (coords[1]..coords[3]).step(coords[3] <=> coords[1]).to_a
+                  Array.new(r.length, coords[0]).zip(r)
+                else
+                  []
+                end
+  end
+
+  def vertical?
+    coords[0] == coords[2]
+  end
+
+  def horizontal?
+    coords[1] == coords[3]
+  end
+end
+
+class Part1
+  def run
+    lines = input.map { |coords| Line.new(coords) }
+
+    lines.map(&:points).flatten(1).tally.count { |_h, k| k > 1 }
+  end
+
+  def input
+    @input ||= $stdin.readlines.map(&:chomp).map { |l| l.split(/\D+/).map(&:to_i) }
+  end
+end
+
+pp Part1.new.run
