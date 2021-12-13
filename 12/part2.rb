c7747aa763b2a6e6b4c9eea3d57fe993dc60911a
Patrick J Cherry <patch@headtube.dominoid.net>
2021-12-13 07:16:13 +0000

    Day 12

---
+++ 12/part2.rb
@@ -0,0 +1,31 @@
+class Part2
+  def run
+    next_step(%w[start]).flatten.count{|s| s == 'start'}
+      # .compact.join(',').gsub('end,',"end\n")
+  end
+
+  def next_step(route)
+    possibles = input.select { |link| link.include?(route.last) }
+
+    twice_visited_small_caves = route.tally.select { |c, count| count == 2 && c =~ /[a-z]/ }.keys
+
+    visited_small_caves = ['start']
+    visited_small_caves += route.select { |c| c =~ /[a-z]/ } if twice_visited_small_caves.any?
+
+    possibles = possibles.flatten - [route.last] - visited_small_caves
+
+    return nil if possibles.empty?
+
+    possibles.map do |cave|
+      next route + [cave] if cave == 'end'
+
+      next_step(route + [cave])
+    end
+  end
+
+  def input
+    @input ||= $stdin.readlines.map(&:chomp).map { |l| l.split('-') }
+  end
+end
+
+puts Part2.new.run
